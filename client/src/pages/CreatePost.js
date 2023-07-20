15ac3ed8539411e90d84778417551f57a2e9c916
Rugved Wagh <89897353+Rugved76@users.noreply.github.com>
2023-07-20 19:22:56 +0530

    Update CreatePost.js

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,3 +1,66 @@
+// import 'react-quill/dist/quill.snow.css';
+// import { useState } from "react";
+// import { Navigate } from "react-router-dom";
+// import Editor from "../components/Editor";
+// import { url } from '../App';
+// import axios from 'axios'
+
+// export default function CreatePost() {
+
+//   const [title, setTitle] = useState('');
+//   const [summary, setSummary] = useState('');
+//   const [content, setContent] = useState('');
+//   const [files, setFiles] = useState('');
+//   const [redirect, setRedirect] = useState(false);
+
+//   async function createNewPost(ev) {
+
+//     const data = new FormData();
+
+//     data.set('title', title);
+//     data.set('summary', summary);
+//     data.set('content', content);
+//     // data.set('file', files[0]);
+
+//     ev.preventDefault();
+//     // try{
+//     //   await axios.post(`${url}/post`)
+//     //   setRedirect(true)
+//     // }catch(er){
+//     //   console.log(er);
+//     // }
+//     const response = await fetch(`${url}/post`, {
+//       method: 'POST',
+//       body: data,
+//       credentials: 'include',
+//     });
+//     if (response.ok) {
+//       setRedirect(true);
+//     }
+  
+//   }
+
+//   if (redirect) {
+//     return <Navigate to={'/'} />
+//   }
+  
+//   return (
+//     <form onSubmit={createNewPost}>
+//       <input type="title"
+//         placeholder={'Title'}
+//         value={title}
+//         onChange={ev => setTitle(ev.target.value)} />
+//       <input type="summary"
+//         placeholder={'Summary'}
+//         value={summary}
+//         onChange={ev => setSummary(ev.target.value)} />
+//       {/* <input type="file"
+//         onChange={ev => setFiles(ev.target.files)} /> */}
+//       <Editor value={content} onChange={setContent} />
+//       <button className="cae" style={{ marginTop: '5px' }}>Create post</button>
+//     </form>
+//   );
+// }
 import 'react-quill/dist/quill.snow.css';
 import { useState } from "react";
 import { Navigate } from "react-router-dom";
@@ -54,8 +117,8 @@ export default function CreatePost() {
         placeholder={'Summary'}
         value={summary}
         onChange={ev => setSummary(ev.target.value)} />
-      // <input type="file"
-      //   onChange={ev => setFiles(ev.target.files)} />
+      {/* // <input type="file"
+        //  onChange={ev => setFiles(ev.target.files)}  */}
       <Editor value={content} onChange={setContent} />
       <button className="cae" style={{ marginTop: '5px' }}>Create post</button>
     </form>
